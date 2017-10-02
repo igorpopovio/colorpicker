582d0f2d8dd07373592248d04be3661d51a14b25
Igor Popov <igor@igorpopov.io>
2017-10-02 14:55:49 +0300

    Add some code
    Signed-off-by: Igor Popov <igor@igorpopov.io>

--- ColorPicker/MainWindow.xaml.cs
+++ ColorPicker/MainWindow.xaml.cs
@@ -1,17 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Point = System.Drawing.Point;
 
 namespace ColorPicker
 {
@@ -20,9 +14,84 @@ namespace ColorPicker
     /// </summary>
     public partial class MainWindow : Window
     {
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetCursorPos(ref Point lpPoint);
+
+
         public MainWindow()
         {
             InitializeComponent();
+            
         }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr GetDesktopWindow();
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr GetWindowDC(IntPtr window);
+        [DllImport("gdi32.dll", SetLastError = true)]
+        public static extern uint GetPixel(IntPtr dc, int x, int y);
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern int ReleaseDC(IntPtr window, IntPtr dc);
+
+        public static Color GetColorAt(int x, int y)
+        {
+            var desktop = GetDesktopWindow();
+            var desktopContext = GetWindowDC(desktop);
+            int pixel = (int)GetPixel(desktopContext, x, y);
+            ReleaseDC(desktop, desktopContext);
+
+            return Color.FromArgb(255,
+                GetColorByte(ColorOffset.RED, pixel),
+                GetColorByte(ColorOffset.GREEN, pixel),
+                GetColorByte(ColorOffset.BLUE, pixel));
+        }
+
+        private static byte GetColorByte(ColorOffset offset, int pixel)
+        {
+            return Convert.ToByte((pixel >> (int)offset) & 0xff);
+        }
+
+        enum ColorOffset
+        {
+            RED = 0,
+            GREEN = 8,
+            BLUE = 16,
+        }
+
+        //private void Window_MouseMove(object sender, MouseEventArgs e)
+        //{
+        //    var p = PointToScreen(Mouse.GetPosition(this));
+        //    var color = GetColorAt((int)p.X, (int)p.Y);
+        //    border.Background = new SolidColorBrush(color);
+        //    e.Handled = false;
+        //}
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //CaptureMouse();
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    Point p = new Point();
+                    GetCursorPos(ref p);
+
+                    Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        var color = GetColorAt(p.X, p.Y);
+                        border.Background = new SolidColorBrush(color);
+                    }));
+
+                    Thread.Sleep(100);
+                }
+            }).Start();
+        }
+
+        //private void Window_Closing(object sender, CancelEventArgs e)
+        //{
+        //    base.OnClosing(e);
+        //    ReleaseMouseCapture();
+        //}
     }
 }
